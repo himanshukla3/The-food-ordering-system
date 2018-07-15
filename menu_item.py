1d45e48caccd728aa6550d9ff5567140c4aec13e
Himanshu Shukla <hmoney701@gmail.com>
2018-07-15 11:46:57 +0530

    Initialized menu-item.py
    progate python study 4

---
+++ menu_item.py
@@ -0,0 +1,16 @@
+class MenuItem:
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+
+    def info(self):
+        return self.name + ': $' + str(self.price)
+
+    def get_total_price(self, count):
+        total_price = self.price * count
+        
+        # If count is 3 or higher, multiply it by 0.9
+        if count >= 3:
+            total_price *= 0.9
+        
+        return round(total_price)
